9085071060b3ed588cdc9bf90d05c4e902225446
Cristóvão Olegário <cristovaoolegario@gmail.com>
2023-03-20 09:26:51 -0300

    chore: Update cmd/main.go with all services from fipe-api

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/cristovaoolegario/fipe-scrapper/internal/pkg/http/services"
 )
 
 func main() {
+	var (
+		brandCode string
+		modelCode string
+	)
 	service := services.NewFipeService(http.Client{
 		Timeout: time.Duration(10) * time.Second,
 	}, services.BASE_URL)
@@ -16,7 +21,6 @@ func main() {
 	references, _ := service.GetAllReferences()
 
 	fmt.Println("\n====== Reference table ======")
-
 	for _, item := range references {
 		fmt.Printf("%s - %d\n", item.Mes, item.Codigo)
 	}
@@ -29,7 +33,8 @@ func main() {
 		fmt.Printf("%s - %s\n", item.Label, item.Value)
 	}
 
-	modelsBrands, _ := service.GetBrandModels(services.Car, brandsDto[0].Value)
+	brandCode = brandsDto[0].Value
+	modelsBrands, _ := service.GetBrandModels(services.Car, brandCode)
 
 	fmt.Println("\n====== Models table ======")
 
@@ -37,4 +42,24 @@ func main() {
 		fmt.Printf("%s - %d\n", item.Label, item.Value)
 
 	}
+
+	fmt.Println("\n====== Models Years table ======")
+
+	modelCode = fmt.Sprint(modelsBrands.Modelos[0].Value)
+	modelYears, _ := service.GetBrandModelsYears(services.Car, brandCode, modelCode)
+	modelYearArr := strings.Split(modelYears[0].Value, "-")
+	year, fuelType := modelYearArr[0], modelYearArr[1]
+
+	for _, item := range modelYears {
+		arr := strings.Split(item.Value, "-")
+		year := arr[0]
+		fuelType := arr[1]
+		fmt.Printf("Year:%s Fuel type: %s\n", year, fuelType)
+	}
+
+	fmt.Println("\n====== Vehicles table ======")
+
+	vehicle, _ := service.GetVehicle(services.Car, brandCode, modelCode, year, fuelType)
+
+	fmt.Printf("%+v", vehicle)
 }
